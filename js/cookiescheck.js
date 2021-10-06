0ed33d996508bc524cc423d26d05e9e56364e252
Deyan Nikolov <dflbg@abv.bg>
2021-10-06 22:46:55 +0300

    1

--- js/cookiescheck.js
+++ js/cookiescheck.js
@@ -2,10 +2,17 @@
 if (window.navigator.cookieEnabled == false) {
     window.location.href = "https://deyanvnikolov.github.io/discoverbulgaria/cookies-disabled.html";
 }else{
-document.cookie = "language=; expires=Thu, 01 Jan 1970 00:00:01 GMT"
-document.cookie = "platform=; expires=Thu, 01 Jan 1970 00:00:01 GMT"
-document.cookie = "onLine=; expires=Thu, 01 Jan 1970 00:00:01 GMT"
-document.cookie = "time=; expires=Thu, 01 Jan 1970 00:00:01 GMT"
+    function clearCookies(wildcardDomain=false, primaryDomain=true, path=null){
+        pathSegment= path ? '; path=' + path : ''
+        expSegment="=;expires=Thu, 01 Jan 1970 00:00:00 GMT"
+        document.cookie.split(';').forEach(
+            function(c) { 
+                primaryDomain && (document.cookie = c.replace(/^ +/, "").replace(/=.*/, expSegment + pathSegment))
+                wildcardDomain && (document.cookie = c.replace(/^ +/, "").replace(/=.*/, expSegment + pathSegment + '; domain=' + document.domain))
+             }
+         )}
+    
+    clearCookies()
 
     document.cookie = "language=" + window.navigator.language + "; secure"
     document.cookie = "platform=" + window.navigator.platform + "; secure" 
