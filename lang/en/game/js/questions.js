11baa325d7a560362f581e367eea42ff6764018c
Deyan Nikolov <dflbg@abv.bg>
2021-11-26 14:45:29 +0200

    Game

---
+++ lang/en/game/js/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Where is 'The Sea Garden' located?",
+    answer: "Varna",
+    options: [
+      "Varna",
+      "Sofia",
+      "Ruse",
+      "Plovdiv"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Where is the 'Ancient Theatre' located?",
+    answer: "Plovdiv",
+    options: [
+      "Shumen",
+      "Burgas",
+      "Varna",
+      "Plovdiv"
+    ]
+  },
+    {
+    numb: 3,
+    question: "In which city can you see Kapana Fest?",
+    answer: "Plovdiv",
+    options: [
+      "Plovdiv",
+      "Veliko Tarnovo",
+      "Ruse",
+      "Burgas"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which is the most populated city?",
+    answer: "Sofia",
+    options: [
+      "Burgas",
+      "Varna",
+      "Sofia",
+      "Plovdiv"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which is the 'Sea Capital' of Bulgaria?",
+    answer: "Varna",
+    options: [
+      "Plovdiv",
+      "Varna",
+      "Burgas",
+      "Ruse"
+    ]
+  },
+
+];
